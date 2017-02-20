7ead33b9c841f1c2c5f62143f1eb23f98009c76f
Ashok Tulachan <tulachanashok@Ashoks-MBP.fios-router.home>
2017-02-20 14:23:31 -0500

    Added package json file

--- index.php
+++ index.php
@@ -334,16 +334,12 @@
             <div class="row">
                 <?php
                 if($_GET['success'] == 1) {
-                    echo "<div class="form-messages success">Thank you! Your message has been sent.</div>";                  
+                    echo "<div class="form-messages success">Thank you! Your message has been sent.</div>";                 
                 }
                 
-                if($_GET['success'] == 1) {
+                if($_GET['success'] == -1) {
                     echo "<div class="form-messages success">Oops! Something went wrong. Please try again.</div>";                  
                 }
-//   
-//                <div class="form-messages error">
-//                    Oops! Something went wrong. Please try again.
-//                </div> 
                 /?>
             </div>
 
